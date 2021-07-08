db7296e32f11c81f45adb03d4f7025d66d2de202
jeffIrvine <jirvine1212@gmail.com>
2021-07-08 16:25:56 -0700

    validation of inputs from req

--- botDataFetch/botDataFetch.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://60c8ed887dafc90017ffbd56.mockapi.io/robots')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 const express = require('express')
 const app = express()
+const axios = require('axios');
+
+app.use(express.json())
+
+app.post('/api/closest-bot', (req, res) => {
+  const data = validateInput(req.body)
+  res.send(data)
+}) 
+
+const validateInput = ({ x: xCord, y: yCord }) => {
+  if(typeof xCord == 'number' && typeof yCord == 'number') {
+    return true 
+  }
+
+  else return false;
+}
+
+const botFetch = () => {
+  axios.get('https://60c8ed887dafc90017ffbd56.mockapi.io/robots')
+  .then(({ data }) => data)
+
+  .catch(error => {
+    console.log(error);
+  })
+}
+  
 
 app.listen(6660, () => console.log('Server started'))
