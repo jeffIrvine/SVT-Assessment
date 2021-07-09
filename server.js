94ed2d8f61a64a598e2b03da5d9dd518219a50c7
jeffIrvine <jirvine1212@gmail.com>
2021-07-08 17:50:41 -0700

    mvp reached monooooolith

--- server.js
+++ server.js
@@ -11,23 +11,28 @@ app.post('/api/closest-bot', async (req, res) => {
   const botPositionData = await botFetch()
 
   for(let i = 0; i < botPositionData.length; i++){
-    const distance = calcDistance(req.body, botPositionData[i])
-    botPositionData[i].distanceToGoal = distance
+    const distanceToGoal = calcDistance(req.body, botPositionData[i])
+    botPositionData[i].distanceToGoal = distanceToGoal
+    delete botPositionData[i].x
+    delete botPositionData[i].y
   }
-  console.log(botPositionData)
-  // botPositionData.sort((a, b) => {
-  //   return calcDistance(req.body, a) - calcDistance(req.body, b)
-  // })
 
-}) 
+  botPositionData.sort((a, b) => a.distanceToGoal - b.distanceToGoal)
+
+  const filteredBots = botPositionData.filter((botItem)=> botItem.distanceToGoal <= 10)
+  if(filteredBots.length === 0) return res.send(botPositionData[0])
+
+  else return res.send(filteredBots.sort((a, b) => b.batteryLevel - a.batteryLevel)[0])
+})
+
 
 const validateInput = ({ x: xCord, y: yCord }) => {
   if(typeof xCord == 'number' && typeof yCord == 'number') {
     return true 
   }
-
   else return false;
 }
+
 
 const calcDistance = (loadPoints, botPoints) => {
   const { x: x1, y: y1} = loadPoints
@@ -35,6 +40,7 @@ const calcDistance = (loadPoints, botPoints) => {
 
   return Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 }
+
 
 const botFetch = () => {
   return axios.get('https://60c8ed887dafc90017ffbd56.mockapi.io/robots')
